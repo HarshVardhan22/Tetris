a8db4cc6d87f8cbf0a42687451ac0026e8bf5944
Harshvardhan <hsinha31@gmail.com>
2020-10-05 11:02:11 +0530

    tetris v1.1

--- script.js
+++ script.js
@@ -41,8 +41,51 @@ document.addEventListener('DOMContentLoaded', ()=>{
       [1, width + 1, width * 2 + 1, width * 3 + 1],
       [width, width + 1, width + 2, width + 3]
     ];
-})
+
 
 const theBlocks = [LBlock,zBlock,tBlock,oBlock,iBlock];
 
-let cur
+let currentPosition =4;
+let currentRotation =0;
+
+//selecting a tetrimino at random
+let random = Math.floor(Math.random()*theBlocks.length);
+
+let current = theBlocks[random][currentRotation];
+
+//Drawing the tetriminos or blocks
+
+function draw(){
+  current.forEach(index=>{
+    squares[currentPosition+index].classList.add('block');
+  })
+}
+
+function undraw(){
+  current.forEach(index=>{
+    squares[currentPosition+index].classList.remove('block');
+  })
+}
+
+timerId = setInterval(moveDown, 200);
+
+function moveDown(){
+  undraw();
+  currentPosition+=width;
+  draw();
+  freeze();
+}
+
+function freeze(){
+  if( current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+    current.forEach(index=>squares[currentPosition + index].classList.add('taken'));
+  
+    // create a new block falling
+  random = Math.floor(Math.random()*theBlocks.length);
+  current = theBlocks[random][currentRotation];
+  currentPosition = 4;
+  draw();
+
+}}
+})
+ 
